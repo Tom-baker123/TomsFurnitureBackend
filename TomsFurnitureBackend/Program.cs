88f125b37b28abaa7f8f1c7a860947200c0ff310
hoangminh203 <hoangminhit203@gmail.com>
2025-05-21 21:53:26 +0700

    Created

--- TomsFurnitureBackend/Program.cs
+++ TomsFurnitureBackend/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CloudinaryDotNet;
+using Microsoft.EntityFrameworkCore;
 using System;
+using TomsFurnitureBackend.Helpers;
 using TomsFurnitureBackend.Models;
 using TomsFurnitureBackend.Services;
+using TomsFurnitureBackend.Services.Interfaces;
 using TomsFurnitureBackend.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,22 +21,29 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<TomfurnitureContext>(options =>
     options.UseSqlServer(connectionString));
 
-
+//3. Services
 builder.Services.AddScoped<IUnitService, UnitService>();
+builder.Services.AddScoped<ISliderService, SliderService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// 4. Cloudinary
+var cloudinarySettings = builder.Configuration.GetSection("CloudinarySettings").Get<CloudinarySettings>();
+builder.Services.AddSingleton(new Cloudinary(new Account(
+    cloudinarySettings.CloudName,
+    cloudinarySettings.ApiKey,
+    cloudinarySettings.ApiSecret
+)));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
---
+++ TomsFurnitureBackend/Services/IServices/ISliderService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using OA.Domain.Common.Models;
+using TomsFurnitureBackend.VModels;
+
+namespace TomsFurnitureBackend.Services.Interfaces
+{
+    public interface ISliderService
+    {
+        Task<ResponseResult> Create(SliderCreateVModel model, string imageUrl);
+        //  Task<ResponseResult> Update(int id, SliderUpdateVModel model);
+        Task<ResponseResult> GetByIdAsync(int id);
+    }
+}
