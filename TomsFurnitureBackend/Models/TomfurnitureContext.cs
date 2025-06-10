baa3bbad85fefc8be8db0b38fca488354d2e6fff
tom <119601473+Tom-baker123@users.noreply.github.com>
2025-06-10 21:07:20 +0700

    Test-3
    Sửa lại đường dẫn

--- TomsFurnitureBackend/Models/TomfurnitureContext.cs
+++ TomsFurnitureBackend/Models/TomfurnitureContext.cs
@@ -79,9 +79,19 @@ public partial class TomfurnitureContext : DbContext
 
     public virtual DbSet<UserGuest> UserGuests { get; set; }
 
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Data Source=ASUS;Initial Catalog=TOMFURNITURE;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=ASUS;Initial Catalog=TOMFURNITURE;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+    {
+        // Connection string is configured in Program.cs via AddDbContext
+        // No need to specify it here unless optionsBuilder is not configured
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Optional: Log or throw an exception if the configuration is missing
+            throw new InvalidOperationException("DbContext options are not configured.");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
